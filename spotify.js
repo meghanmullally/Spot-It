b020d55044b50af041e8f25552aa65ad60fd4cc7
Meghan Mullally <ms.mullally27@gmail.com>
2019-07-16 10:32:42 -0700

    added localhost:8888 redirct url -  will change later

--- spotify.js
+++ spotify.js
@@ -2,21 +2,29 @@
 //  https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
 //  FOR NOTES: https://github.com/spotify/web-api-auth-examples
 
+require("dotenv").config();
+
+
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
 
-/**
+// client id
+var client_id = process.env.CLIENT_ID; 
+// secret id
+var client_secret = process.env.CLIENT_SECRET; 
+// redirect uri
+var redirect_uri = process.env.REDIRECT_URI; 
+
+/*
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
  * @return {string} The generated string
  */
+
 var generateRandomString = function (length) {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
