11cbf0f8d8ecf6e165055b57d9f0c54146dc5e34
Meghan Mullally <ms.mullally27@gmail.com>
2019-07-15 22:20:38 -0700

    added boiler plate code for spotify authorization log in...create an spotify.html page for the login display

--- server.js
+++ server.js
@@ -44,4 +44,21 @@ db.sequelize.sync(syncOptions).then(function() {
   });
 });
 
+
+
+// spotify api 
+
+  /* Load the HTTP library */
+  var http = require("http");
+
+  /* Create an HTTP server to handle responses */
+
+  http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }).listen(8888);
+
+
+  
 module.exports = app;
