3d639b58aa461dd5cdc34e47c1030214efba68da
Code-SY <48194849+Code-SY@users.noreply.github.com>
2019-07-18 00:23:27 -0700

    Added new set of hbs files, fixed issues with login,signup,profile (#13)

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 require("dotenv").config();
+var path = require('path');
 var express = require("express");
 var exphbs = require("express-handlebars");
 var session = require('express-session');
+var validator = require('express-validator');
 var passport = require("./config/passport");
+const flash = require('connect-flash');
 
 var db = require("./models");
 
 var app = express();
-var PORT = process.env.PORT || 3004;
+var PORT = process.env.PORT || 3008;
 
 // Middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -21,35 +26,51 @@ app.use(session({
   resave: true
 }));
 
+app.use(flash());
 // Passport
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(validator());
 
 // Handlebars
 app.engine(
-  "handlebars",
+  "hbs",
   exphbs({
-    defaultLayout: "main"
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+    //helpers: require('./lib/handlebars')
   })
 );
-app.set("view engine", "handlebars");
+app.set("view engine", "hbs");
+
+// Global variables
+app.use((req, res, next) => {
+  app.locals.message = req.flash('message');
+  app.locals.success = req.flash('success');
+  app.locals.user = req.user;
+  next();
+});
 
 // Routes
 require("./routes/userRoutes")(app);
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: true };
+var syncOptions = {
+  force: false
+};
 
 // If running a test, set syncOptions.force to true
-// clearing the `testdb`
+// clearing the database
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
@@ -58,5 +79,5 @@ db.sequelize.sync(syncOptions).then(function() {
   });
 });
 
-  
+
 module.exports = app;
