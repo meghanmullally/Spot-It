142e0421bbccfb1ab6462f9c6b7e54409af4cf51
Meghan Mullally <ms.mullally27@gmail.com>
2019-07-18 14:01:06 -0700

    removed SpotifyRoutes and routes()

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
-// require("./routes/spotifyRoutes");
+
 // require("./routes/htmlRoutes")(app);
 
 var syncOptions = {
